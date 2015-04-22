d079cee03caa127ece2ef9b5dbf3dc1a4395478d
Mikey Clarke <epicschnozz@gmail.com>
2015-04-22 21:43:03 +1200

    Trying Less Secure Gmail, plus domain=gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               '119.9.42.4',
+    domain:               'gmail.com',
     user_name:            'epicschnozz@gmail.com',
     password:             'blaughblaugh',
     authentication:       'plain',
