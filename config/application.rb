98a17e42a36ab980455484835e3facedcec1eec5
Mikey Clarke <epicschnozz@gmail.com>
2015-04-22 20:49:24 +1200

    Moved smtp to production.rb, and changed smtp domain to IP address

--- config/application.rb
+++ config/application.rb
@@ -23,15 +23,5 @@ module Shipping
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'epicschnozz@gmail.com',
-      password:             'blaughblaugh',
-      authentication:       'plain',
-      enable_starttls_auto: true
-    }
   end
 end
