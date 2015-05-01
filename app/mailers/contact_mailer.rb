85583e3a7343c9c5647b2482f4b67536b017bc6a
Mikey Clarke <epicschnozz@gmail.com>
2015-05-02 00:53:29 +1200

    And back to annabel.young@shipfed.co.nz

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,6 +7,6 @@ class ContactMailer < ApplicationMailer
     @message = params[:message]
 
     # Actual email to send: annabel.young@shipfed.co.nz
-    mail to: 'epicschnozz@gmail.com', from: params[:email], subject: "NZSF Inquiry from #{params[:name]}"
+    mail to: 'annabel.young@shipfed.co.nz', from: params[:email], subject: "NZSF Inquiry from #{params[:name]}"
   end
 end
