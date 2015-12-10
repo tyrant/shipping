b16db947be3d7effedb9d23cdbb0458d990cd670
Mikey Clarke <epicschnozz@gmail.com>
2015-12-10 20:28:46 +1300

    Odd error, rbenv on production is saying ruby 2.2.0 isn't installed

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :rbenv_type, :user
 
 namespace :deploy do
 
