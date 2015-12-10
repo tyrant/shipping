4e77716af35a8c4a4cd772eaecca26a07a1afb7a
Mikey Clarke <epicschnozz@gmail.com>
2015-12-10 20:40:35 +1300

    Removed rbenv calls in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,9 +39,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.2.0'
-
 namespace :deploy do
 
   desc "Restart application"
