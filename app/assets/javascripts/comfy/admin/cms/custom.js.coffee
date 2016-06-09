2dbd800d5de7b0ff43710e53a5a9fb463964e34a
Mikey Clarke <epicschnozz@gmail.com>
2016-06-09 15:09:37 +1200

    More upgrades, added Media subpages, discovered Redactor is now default, and it's an unforgiving bastard to use

--- app/assets/javascripts/comfy/admin/cms/custom.js.coffee
+++ app/assets/javascripts/comfy/admin/cms/custom.js.coffee
@@ -1,1 +1,27 @@
 # Custom JS for the admin area
+
+#= require comfy/admin/cms/inlinestyle
+#= require comfy/admin/cms/fontcolor
+
+window.CMS.wysiwyg = ->
+  csrf_token = $('meta[name=csrf-token]').attr('content')
+  csrf_param = $('meta[name=csrf-param]').attr('content')
+
+  if (csrf_param != undefined && csrf_token != undefined)
+    params = csrf_param + "=" + encodeURIComponent(csrf_token)
+
+  $('textarea.rich-text-editor, textarea[data-cms-rich-text]').redactor
+    autoresize:       true
+    imageUpload:      "#{CMS.file_upload_path}?source=redactor&type=image&#{params}"
+    imageManagerJson: "#{CMS.file_upload_path}?source=redactor&type=image"
+    fileUpload:       "#{CMS.file_upload_path}?source=redactor&type=file&#{params}"
+    fileManagerJson:  "#{CMS.file_upload_path}?source=redactor&type=file"
+    #definedLinks:     "#{CMS.pages_path}?source=redactor"
+    buttonSource:     true
+    #formatting:       ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+    plugins:          ['imagemanager', 'fontcolor', 'inlinestyle', 'filemanager', 'table', 'video', 'definedlinks']
+    lang:             CMS.locale
+    convertDivs:      false
+    minHeight:        350
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
 
+  # The most recent five news pages.
   def recent_news
 
     # Grab the sub-pages of the 'news' page...
@@ -11,14 +12,14 @@ module ApplicationHelper
 
       # ...order them by either published or creation date, descending...
       end.sort do |page1, page2|                   
-        news_published_on(page2).to_i <=> news_published_on(page1).to_i
+        page_published_on(page2).to_i <=> page_published_on(page1).to_i
 
       # ...grab the first five and return their label and creation date.
       end.take(5).map do |page|            
         {
           label: page.label,
           slug:  page.slug, 
-          date:  news_published_on(page).strftime('%F')
+          date:  page_published_on(page).strftime('%F')
         }
       end
     else 
@@ -26,8 +27,32 @@ module ApplicationHelper
     end
   end
 
+  # The most recent five media pages.
+  def recent_media
+
+    media_page = Comfy::Cms::Page.where(label: 'Media').first
+
+    if media_page
+      media_page.children.find_all do |page|
+        page.is_published?
+
+      end.sort do |page1, page2|
+        page_published_on(page2).to_i <=> page_published_on(page1).to_i
+
+      end.take(5).map do |page|
+        {
+          label: page.label,
+          slug:  page.slug,
+          date:  page_published_on(page).strftime('%F')
+        }
+      end
+    else
+      []
+    end
+  end
+
   # Given a news article, return (if it has one), its published_on block content, otherwise its created_at.
-  def news_published_on(page)
+  def page_published_on(page)
     published_on = page.blocks.find {|b| b.identifier == 'published_on' }
     if !!published_on && !published_on.content.blank?
       DateTime.parse published_on.content
@@ -36,11 +61,11 @@ module ApplicationHelper
     end
   end
 
-  # Filter the site files by whether they're categorised as 'ships', and return their URLs.
+  # Filter the site files by whether they're categorised as 'ships', randomise their order, and return their URLs.
   def ship_images
     @cms_site.files.find_all do |file|
       file.categories.map {|c| c.label }.include? 'ships'
-    end
+    end.shuffle!
   end
 
   def current_link_to(content, url, options={})
@@ -60,5 +85,10 @@ module ApplicationHelper
 
   def about_nzsf_subpages
     Comfy::Cms::Page.find_by_label('About the New Zealand Shipping Federation').children
+  end
+
+  # Is the current page (the Media page or one of its children), or not?
+  def media_page?
+    @cms_page.parent.present? and (@cms_page.slug == 'media' || @cms_page.parent.slug == 'media')
   end
 end
