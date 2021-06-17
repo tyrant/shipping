1e8b5498050cce9c03cbee692998f9e595834004
Mikey Clarke <epicschnozz@gmail.com>
2021-06-17 19:15:47 +1200

    Added agnostic email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,7 +6,6 @@ class ContactMailer < ApplicationMailer
     @phone = params[:phone]
     @message = params[:message]
 
-    # Actual email to send: annabel.young@shipfed.co.nz
-    mail to: 'annabel.young@shipfed.co.nz', from: params[:email], subject: "NZSF Inquiry from #{params[:name]}"
+    mail to: 'admin@nzsf.org.nz', from: params[:email], subject: "NZSF Inquiry from #{params[:name]}"
   end
 end
